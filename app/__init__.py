297729c3754fd4df270f2ceb12ed7ba96b0ab158
jw <wes@pseudokoder.com>
2026-05-20 14:17:43 -0500

    Initialize basic file setup and webpage.

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+import os
+
+from flask import Flask
+from flask_bootstrap import Bootstrap5
+
+def create_app():
+  app = Flask(__name__)
+  Bootstrap5(app)
+
+  # Config
+  app.config["SECRET_KEY"] = 'dev-key-change-later'
+
+  # Register blueprints
+  from app.routes.main import main_bp
+  app.register_blueprint(main_bp)
+
+  return app
